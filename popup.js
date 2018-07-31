55a66a1de17ff6b3c5c5679e65263bca9a1731ec
zhubo <zhubo@microstrategy.com>
2018-07-31 17:06:09 +0800

    add hkex query

--- popup.js
+++ popup.js
@@ -1,11 +1,5 @@
-function myFunction()
+function RegressionFunction()
 {
-  /* 得到当前tab的url
-  chrome.tabs.getSelected(null, function (tab) {
-        console.log(tab.url);
-        console.log(document.body);
-  });
-  */
   chrome.tabs.executeScript(null, {file: "getPagesSource.js"}, function() {
     // If you try and inject into an extensions page or the webstore/NTP you'll get an error
     if (chrome.runtime.lastError) {
@@ -50,15 +44,53 @@ function myFunction()
       })
       var out_win = 'w_s'+windows_succeed+ ' w_na'+windows_NA+ ' w_local'+windows_local+ ' w_sysem'+windows_system_validation
       var out_lin = 'lin_s'+linux_secceed+' lin_na'+linux_NA+' l_local'+linux_local+ ' l_system'+linux_system_validation
-      $("#lin").html(out_lin)
+      var succeed = windows_succeed+linux_secceed;
+      var invalidation = windows_local+linux_local+windows_system_validation+linux_system_validation;
+      var percentage = succeed/(succeed+invalidation);
+      $("#lin").html(out_lin);
       $("#win").html(out_win);
+      $("#per").html(percentage);
 
+    }
+  });
+}
+
+function HKEXFunction()
+{
+  chrome.tabs.executeScript(null, {file: "getPagesSource.js"}, function() {
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+  chrome.runtime.onMessage.addListener(function(request, sender) {
+    var mycars=new Array()
+    mycars[0]="72341"
+    mycars[1]="70333"
+    if (request.action == "getSource") {
+      var div = $(request.source).find("#pnlResult")[0]
+      var table = $(div).find("tbody")[0]
+      //$("#result").html(table);
+      all = "<table>"
+      $(table).find("tr").each(function(i, val){
+          var row0 = $(val).hasClass("row0")
+          var row1 = $(val).hasClass("row1")
+          if (row0||row1) {
+            number = $(val).find("td")[0].innerText
+            if ($.inArray($.trim(number), mycars)!=-1)
+              all = all + "<tr>" + $(val).html() + "</tr>"
+          }
+      })
+      all = all + "</table>"
+      $("#result").html(all);
     }
   });
 }
 
 $(document).ready(function(){
   $('#b1').click(function () {
-      myFunction();
+      RegressionFunction();
+  });
+  $('#b2').click(function () {
+      HKEXFunction();
   });
 })
